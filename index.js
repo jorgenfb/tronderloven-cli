53e5a8949a3e619c48c0f444a5288f1048c1fa1a
Jørgen Borgesen <joergen.borgesen@gmail.com>
2016-06-17 15:32:55 +0200

    Adds argument support for spesifying number of quotes to show

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 
+var program = require('commander');
 var data = require('./data.json');
 
-function randomQuote() {
+program
+	.option('-n, --number <n>', 'Number of quotes to print', parseInt)
+	.parse(process.argv);
+
+var count = program.number || 1;
+
+var result = [];
+while(data.length > 0 && count > 0) {
 	var index = Math.floor(data.length * Math.random());
-	return data[index];
+	var quote = data.splice(index, 1)[0];
+	result.push(quote);
+	count--;
 }
 
-console.log(randomQuote());
+result.forEach(function(r) {
+	console.log(r);
+});
+
